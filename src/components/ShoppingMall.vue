593abb008740fff3a1d0c900ae65bd0df7859a8f
gqm <gqm@joiway.com>
2020-03-06 10:32:24 +0800

    空白项目 引入vant rem

---
+++ src/components/ShoppingMall.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ShoppingMall">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingMall",
+  data() {
+    return {
+      msg: "hello world"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
