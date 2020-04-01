b8c91ec61c3737ad1564333a6e171fdfa9a5603b
guoqianmin <838169412@qq.com>
2020-04-01 20:44:57 +0800

    json文件读取

--- service/index.js
+++ service/index.js
@@ -15,13 +15,13 @@ const user = require('./appApi/user.js')
 const indexData = require('./database/indexData')
 
 !(async () => {
-    await connect()
-    initSchemas()
-    // const User = mongoose.model('User')
-    // let oneUser = new User({ userName: 'lala2', password: '123456' })
-    // oneUser.save().then(() => {
-    //     console.log('插入成功')
-    // })
+  await connect()
+  initSchemas()
+  // const User = mongoose.model('User')
+  // let oneUser = new User({ userName: 'lala2', password: '123456' })
+  // oneUser.save().then(() => {
+  //     console.log('插入成功')
+  // })
 })()
 
 router.use('/user', user.routes())
@@ -31,38 +31,45 @@ app.use(router.routes())
 app.use(router.allowedMethods())
 
 app.use(async (ctx) => {
-    if (ctx.url === '/index/data' && ctx.method === 'GET') {
-        // ctx.body = indexData
-        let filePath = path.join(__dirname, 'indexData.json')
-        console.log(filePath)
-        fs.readFile(filePath, 'utf-8', function (err, data) {
-            console.log(data)
-            if (!err) {
-                ctx.body = {
-                    code: 200,
-                    data: data
-                }
-            } else {
-                console.log(err)
-                ctx.body = {
-                    code: 500,
-                    msg: '请求失败'
-                }
-            }
-            // ctx.body = {
-            //     code: 500,
-            //     msg: file
-            // }
-        })
-        console.log(1234)
-        // ctx.body = {
-        //     code: 500,
-        //     msg: file
-        // }
-    }
+  if (ctx.url === '/index/data' && ctx.method === 'GET') {
+    // ctx.body = indexData
+    let filePath = path.join(__dirname, 'indexData.json')
+    console.log(filePath)
+    let res = await getFileData(filePath)
+    ctx.body = res
+
+    // let data = fs.readFileSync(filePath, 'utf8')
+    // ctx.body = {
+    //   code: 200,
+    //   data: JSON.parse(data),
+    //   msg:'成功',
+    // }
+  }
 })
 
+const getFileData = async function (filePath) {
+  return new Promise((resovle, reject) => {
+    fs.readFile(filePath, 'utf-8', function (err, data) {
+      if (!err) {
+        let res = {
+          code: 200,
+          data: JSON.parse(data),
+          msg: '请求成功'
+        }
+        resovle(res)
+      } else {
+        console.log(err)
+        let error = {
+          code: 500,
+          data: {},
+          msg: '读取失败'
+        }
+        resovle(error)
+      }
+    })
+  })
+}
 
-app.listen(3000, () => {
-    console.log('[Server] starting at port 3000')
+app.listen(3001, () => {
+  console.log('[Server] starting at port 3000')
 })
